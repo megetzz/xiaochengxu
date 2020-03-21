2a97e3d756c39c2cb8baed66462833989296f8b0
megetzz <ma.1971250916@qq.com>
2020-03-21 11:58:58 +0800

    测试

--- pages/ceshi/ceshi.js
+++ pages/ceshi/ceshi.js
@@ -9,8 +9,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    array: [
-      {
+    array: [{
         message: "hello,world"
       },
       {
@@ -25,123 +24,158 @@ Page({
     ]
   },
   // 点击
-  click:function(){
+  click: function() {
     console.log('点击')
   },
 
   // 网络测试
-  test_network:function(){
-    var data=undefined
+  test_network: function() {
+    var data = undefined
     wx.request({
-      url:'https://www.bilibili.com',
-      method:"GET",
-      header:{},
-      success:function(res){
+      url: 'https://www.bilibili.com',
+      method: "GET",
+      header: {},
+      success: function(res) {
         data = res.data
-        console.log('网络请求成功--->',res.data)
+        console.log('网络请求成功--->', res.data)
       },
-      fail:function(res){
-        console.log('网络请求失败',res.errMsg)
+      fail: function(res) {
+        console.log('网络请求失败', res.errMsg)
       }
     })
-    console.log('data:---->',data)
+    console.log('data:---->', data)
   },
-  testnetwork1:function(){
+  testnetwork1: function() {
     // 异步
-    var result='undefined'
+    var result = 'undefined'
     wx.request({
       // url: 'https://pyq.shadiao.app/',
       // url:'http://httpbin.org/',
       // url: 'http://www.jd.com',
       // url:'https://python.org/',
-      url:'http://127.0.0.1:8000/blog/index/',
-      method:"GET",
-      header:{},
+      url: 'http://127.0.0.1:8000/blog/index/',
+      method: "GET",
+      header: {},
       //success和fail是耗时操作
-      success:function(res){
+      success: function(res) {
         result = res.data
         console.log('请求成功')
-        console.log('结果成功:'+result)
+        console.log('结果成功:' + result)
       },
-      fail:function(res){
+      fail: function(res) {
         console.log('结果失败:')
         console.log(res.errMsg)
       }
     })
-    console.log('result:--->',result)
+    console.log('result:--->', result)
   },
 
   // 失败的上传文件
-  shangchuan:function(){
+  shangchuan: function() {
     wx.uploadFile({
       url: 'upload',
       filePath: 'pages/ceshi/abc.jpg',
       name: 'file',
-      fromData : {
-        'user':'test'
+      fromData: {
+        'user': 'test'
       },
       success(res) {
         console.log(res.data)
       },
-      fail(res){
+      fail(res) {
         console.log('上传失败')
         console.log(res.errMsg)
       }
     })
   },
+
+  save: function() {
+    console.log('点击保存数据')
+    wx.setStorage({
+      key: 'ceshi',
+      data: '被保存的数据',
+    })
+  },
+  read: function() {
+    console.log('开始读取数据')
+    wx.getStorage({
+      key: 'ceshi',
+      success: function(res) {
+        console.log('读取数据成功:', res.data)
+      },
+      fail: function(res) {
+        console.log('读取数据失败', res.errMsg)
+      }
+    })
+  },
+  remove: function() {
+    console.log('移除数据')
+    wx.removeStorage({
+      key: 'ceshi',
+      success: function(res) {
+        console.log('remove success')
+        console.log('remove缓存:成功')
+      },
+    })
+  },
+clear:function(){
+  console.log('开始清除所有数据')
+  wx.clearStorage()
+  console.log('clearing......')
+},
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log('测试页加载')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     console.log('测试页读取')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     console.log('测试页显示')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     console.log('测试页隐藏')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
     console.log('用户下拉')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
